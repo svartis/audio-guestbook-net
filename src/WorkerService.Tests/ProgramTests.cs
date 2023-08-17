ff365e6ef11eca1fa0b7128465477a3ad9fc6de4
Stefan Mattsson <stefan.mattsson@if.se>
2023-08-17 16:35:56 +0200

    Added sample test

---
+++ src/WorkerService.Tests/ProgramTests.cs
@@ -0,0 +1,24 @@
+using AudioGuestbook.WorkerService.Services;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AudioGuestbook.WorkerService.Tests;
+
+public class ProgramTests
+{
+    [Fact]
+    public void Program_CreateHostBuilder_HasRequiredServices()
+    {
+        // Setup
+        // When running this test it thinks it's in Production, so force it to Development
+        Environment.SetEnvironmentVariable("DOTNET_ENVIRONMENT", "Development");
+        using var host = Program.CreateHostBuilder(Array.Empty<string>()).Build();
+
+        // Act
+        var serviceProvider = host.Services.GetRequiredService<IServiceProvider>();
+        var appStatus = serviceProvider.CreateScope().ServiceProvider.GetRequiredService<IAppStatus>();
+
+        // Assert
+        appStatus.Should().NotBeNull();
+    }
+}
--- src/WorkerService/Program.cs
+++ src/WorkerService/Program.cs
@@ -1,27 +1,26 @@
-using AudioGuestbook.WorkerService;
 using AudioGuestbook.WorkerService.Services;
 
 //TODO: Seal all classes
 
-try
-{
-    var host = Host.CreateDefaultBuilder(args)
-        .ConfigureServices((_, services) =>
-        {
-            services
-                .AddSingleton<IAppStatus, AppStatus>()
-                .AddSingleton<IGpioAccess, GpioAccess>()
-                .AddSingleton<IAudioOutput, AudioOutput>()
-                .AddHostedService<LedStatusWorker>()
-                .AddHostedService<ProcessWorker>();
+namespace AudioGuestbook.WorkerService;
 
-        })
-        .Build();
-
-    host.Run();
-}
-catch (Exception ex)
+internal static class Program
 {
-    Console.WriteLine(ex.ToString());
-    Console.ReadLine();
+    internal static async Task Main(string[] args)
+    {
+        using var host = CreateHostBuilder(args).Build();
+        await host.RunAsync();
+    }
+
+    internal static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((_, services) =>
+            {
+                services
+                    .AddSingleton<IAppStatus, AppStatus>()
+                    .AddSingleton<IGpioAccess, GpioAccess>()
+                    .AddSingleton<IAudioOutput, AudioOutput>()
+                    .AddHostedService<LedStatusWorker>()
+                    .AddHostedService<ProcessWorker>();
+            });
 }
