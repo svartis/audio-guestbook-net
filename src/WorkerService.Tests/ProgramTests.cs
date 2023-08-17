e21bd1e63271304627fc64edf8ed5807f2e330e2
Stefan Mattsson <stefan.mattsson@if.se>
2023-08-17 16:45:31 +0200

    Added sample test to test code coverage

---
+++ src/WorkerService.Tests/ProgramTests.cs
@@ -0,0 +1,25 @@
+﻿using AudioGuestbook.WorkerService.Services;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace AudioGuestbook.WorkerService.Tests;
+
+public class ProgramTests
+{
+    [Fact]
+    public void Program_CreateHostBuilder_HasRequiredServices()
+    {
+        // Setup
+        // When running this test it thinks it's in Production, so force it to Development
+        Environment.SetEnvironmentVariable("DOTNET_ENVIRONMENT", "Development");
+        using var host = Program.CreateHostBuilder(Array.Empty<string>()).Build();
+
+        // Act
+        var serviceProvider = host.Services.GetRequiredService<IServiceProvider>();
+        var appStatus = serviceProvider.CreateScope().ServiceProvider.GetRequiredService<IAppStatus>();
+
+        // Assert
+        appStatus.Should().NotBeNull();
+    }
+}
