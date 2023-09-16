ff6f11732954922c7352d2183b00fc0628dd3a6a
SM <mr.stefan.mattsson@gmail.com>
2023-09-17 00:32:48 +0200

    Cleanup

--- src/WorkerService/Program.cs
+++ src/WorkerService/Program.cs
@@ -2,15 +2,6 @@ using AudioGuestbook.WorkerService.Services;
 using System.Device.Gpio;
 using Microsoft.Extensions.Options;
 
-//https://learn.microsoft.com/en-us/dotnet/core/extensions/windows-service?pivots=dotnet-7-0
-//sc.exe create "AudioGuestbook Service" binpath="C:\Path\To\App.WindowsService.exe"
-//sc qfailure "AudioGuestbook Service"
-//sc.exe failure "AudioGuestbook Service" reset=0 actions=restart/60000/restart/60000/run/1000
-//TODO: Bat file to auto install sc.exe create ".NET Joke Service" binpath="C:\Path\To\App.WindowsService.exe"
-//sc.exe start "AudioGuestbook Service"
-//sc.exe stop "AudioGuestbook Service"
-//sc.exe delete "AudioGuestbook Service"
-
 namespace AudioGuestbook.WorkerService;
 
 internal static class Program
@@ -35,10 +26,6 @@ internal static class Program
                     .AddSingleton<IAudioOutput, AudioOutput>()
                     .AddSingleton<IAudioRecorder, AudioRecorder>()
                     .AddHostedService<LedStatusWorker>()
-                    .AddHostedService<ProcessWorker>()
-                    .AddWindowsService(options =>
-                    {
-                        options.ServiceName = "AudioGuestbook Service";
-                    });
+                    .AddHostedService<ProcessWorker>();
             });
 }
